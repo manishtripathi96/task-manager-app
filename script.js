0819b7729739f124eb4d780bf2931050a19eb4a1
Manish Tripathi <m.tripathi1996@gmail.com>
2026-03-05 19:16:47 +0000

    Update JS file with ability to create new task

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let newTask = document.getElementById('newTask').value;
+
 let addTaskBtn = document.getElementById('addTask');
 addTaskBtn.addEventListener('click', function(){
-    
+    let text = document.getElementById('newTask').value;
+    let newItem = document.createElement('li');
+    newItem.textContent = text;
+    let taskList = document.getElementById('list');
+    taskList.appendChild(newItem);
 })
